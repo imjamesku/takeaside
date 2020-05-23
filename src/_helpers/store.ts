4af11ae59c1806f28edcbd2a5306ca54621a9836
James Ku <jameskutw@gmail.com>
2020-05-23 11:29:15 -0400

    use redux logger only in development

--- src/_helpers/store.ts
+++ src/_helpers/store.ts
@@ -1,15 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
+import logger, { createLogger } from 'redux-logger';
 import rootReducer from '../_reducers';
 
-
 const loggerMiddleware = createLogger();
+const middlewares: Array<any> = [thunkMiddleware]
+if (process.env.NODE_ENV === "development") {
+    middlewares.push(loggerMiddleware)
+
+}
 
 export const store = createStore(
     rootReducer,
-    applyMiddleware(
-        thunkMiddleware,
-        loggerMiddleware
-    )
+    applyMiddleware(...middlewares)
 )
