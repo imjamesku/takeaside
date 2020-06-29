e7503def0e39546a5040aea169b8a22ecacedc37
James Ku <jameskutw@gmail.com>
2020-06-28 22:08:23 -0400

    refresh page after creating topic

--- src/components/CreateTopicForm/CreateTopicForm.tsx
+++ src/components/CreateTopicForm/CreateTopicForm.tsx
@@ -1,59 +1,78 @@
-import React, { useState, FormEvent } from 'react'
-import styles from './CreateTopicForm.module.scss'
-import {mutate} from 'swr'
-import { topicService } from '../../_services/topic_service';
-import Topic from '../../_types/Topic';
-import { useGoogleReCaptcha } from "react-google-recaptcha-v3"
+import React, { useState, FormEvent } from "react";
+import styles from "./CreateTopicForm.module.scss";
+import { mutate } from "swr";
+import { topicService } from "../../_services/topic_service";
+import Topic from "../../_types/Topic";
+import { useGoogleReCaptcha } from "react-google-recaptcha-v3";
 
-interface Props {
-    
-}
+interface Props {}
 
 const CreateTopicForm = (props: Props) => {
+  const [formData, setFormData] = useState({
+    question: "",
+    left: "",
+    right: "",
+  });
+  const { executeRecaptcha } = useGoogleReCaptcha();
+  const [alert, setAlert] = useState("");
 
-    const [formData, setFormData] = useState({
-        question: '',
-        left: '',
-        right: ''
-    })
-    const {executeRecaptcha} = useGoogleReCaptcha()
-    const [alert, setAlert] = useState('')
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+    setFormData((inputs) => ({ ...inputs, [name]: value }));
+  }
 
-    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-        const { name, value } = e.target;
-        setFormData(inputs => ({ ...inputs, [name]: value }));
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    setAlert("");
+    e.preventDefault();
+    if (!executeRecaptcha) {
+      window.alert("something went wrong");
+      return;
     }
+    const token = await executeRecaptcha("createTopic");
+    topicService
+      .createTopic(formData, token)
+      .then(() => {
+        setFormData({ question: "", left: "", right: "" });
+        setAlert("Topic Created");
+        location.reload();
+      })
+      .catch((error) => {
+        setAlert("Failed to create the topic");
+      });
+  }
+  return (
+    <div className={styles.createTopic}>
+      <h1>Create Topic</h1>
+      {alert && <span className={styles.alert}>{alert}</span>}
+      <form action="" className={styles.form} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="question"
+          value={formData.question}
+          onChange={handleChange}
+          placeholder="Question"
+          required
+        />
+        <input
+          type="text"
+          name="left"
+          placeholder="First Option"
+          value={formData.left}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="text"
+          name="right"
+          placeholder="Second Option"
+          value={formData.right}
+          onChange={handleChange}
+          required
+        />
+        <button>Create</button>
+      </form>
+    </div>
+  );
+};
 
-    async function handleSubmit(e: FormEvent<HTMLFormElement>) {
-        setAlert('')
-        e.preventDefault()
-        if (!executeRecaptcha) {
-            window.alert("something went wrong")
-            return
-        }
-        const token = await executeRecaptcha("createTopic")
-        topicService.createTopic(formData, token)
-            .then((newTopic: any) => {
-                mutate('/topics', (topics: Array<Topic>) => topics ? [newTopic, ...topics] : [newTopic])
-                setFormData({question: '', left: '', right: ''})
-                setAlert('Topic Created')
-            })
-            .catch(error => {
-                setAlert('Failed to create the topic')
-            })
-    }
-    return (
-        <div className={styles.createTopic}>
-            <h1>Create Topic</h1>
-            {alert && <span className={styles.alert}>{alert}</span>}
-            <form action="" className={styles.form} onSubmit={handleSubmit}>
-                <input type="text" name="question" value={formData.question} onChange={handleChange} placeholder="Question" required />
-                <input type="text" name="left" placeholder="First Option" value={formData.left} onChange={handleChange} required/>
-                <input type="text" name="right" placeholder="Second Option" value={formData.right} onChange={handleChange} required/>
-                <button>Create</button>
-            </form>
-        </div>
-    )
-}
-
-export default CreateTopicForm
+export default CreateTopicForm;
