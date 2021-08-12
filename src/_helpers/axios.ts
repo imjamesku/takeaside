504f1fbf33db0c087caf9cd1d5676cc0fc731284
James Ku <jameskutw@gmail.com>
2021-08-12 21:48:44 +0800

    Extend timeout to 6 seconds
    Since serverless architecture sometimes responds slower

--- src/_helpers/axios.ts
+++ src/_helpers/axios.ts
@@ -1,23 +1,23 @@
-import axios, { AxiosRequestConfig } from 'axios'
-import parseJSON from './parseJSON'
+import axios, { AxiosRequestConfig } from "axios";
+import parseJSON from "./parseJSON";
 
 const instance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
-    timeout: 1000,
-    headers: {'Content-Type': 'application/json'}
-})
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  timeout: 6000,
+  headers: { "Content-Type": "application/json" },
+});
 
 instance.interceptors.request.use((config: AxiosRequestConfig) => {
-    const userStr = localStorage.getItem('user')
-    if (!userStr) {
-        return config
-    }
-    const user = parseJSON(userStr)
-    if (user) {
-        const token = user.token
-        config.headers.Authorization = `Bearer ${token}`
-    } 
-    return config
-})
+  const userStr = localStorage.getItem("user");
+  if (!userStr) {
+    return config;
+  }
+  const user = parseJSON(userStr);
+  if (user) {
+    const token = user.token;
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
-export default instance
+export default instance;
